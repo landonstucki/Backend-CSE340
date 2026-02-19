46bfe66cf641f322f64698a02a4a22165d00ea70
Landon <Landon@users.noreply.github.com>
2026-02-18 21:46:31 -0700

    attempt trust proxy

--- server.js
+++ server.js
@@ -22,6 +22,7 @@ const PORT = process.env.PORT || 3000;
  * Setup Express Server
  */
 const app = express();
+app.set('trust proxy', 1);
 
 // Initialize PostgreSQL session store
 const pgSession = connectPgSimple(session);
